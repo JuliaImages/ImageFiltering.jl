a635994a95413a2aaf72574a861e773d5ead49ad
Júlio Hoffimann <julio.hoffimann@gmail.com>
2016-08-24 19:55:44 -0500

    Add imgradients
    Taken from Images pull request 544

--- src/specialty.jl
+++ src/specialty.jl
@@ -14,3 +14,78 @@ function _imfilter_inbounds!(r, out, A::AbstractArray, L::Laplacian, border::NoP
     end
     out
 end
+
+## imgradients
+"""
+    imgradients(img, [points], [method], [border]) -> g1, g2, ...
+
+Performs edge detection filtering of the N-dimensional array `img`.
+Gradients are computed at specified `points` (or indexes) in the
+array or everywhere. Available methods: `"sobel"` and `"ando3"`.
+Border options:`"replicate"`, `"circular"`, `"reflect"`, `"symmetric"`.
+Returns a 2D array `G` with the gradients as rows. The number of rows
+is the number of points at which the gradient was computed and the
+number of columns is the dimensionality of the array.
+"""
+function imgradients{T,N}(img::AbstractArray{T,N}, points::AbstractVector;
+                          method::AbstractString="ando3", border::AbstractString="replicate")
+    extent = size(img)
+    ndirs = length(extent)
+    npoints = length(points)
+
+    # pad input image only on appropriate directions
+    imgpad = _gradientpad(img, border)
+
+    # gradient matrix
+    Tret = typeof(zero(T)*zero(Float64))
+    G = zeros(Tret, npoints, ndirs)
+
+    for dir in 1:ndirs
+        # kernel = centered difference + perpendicular smoothing
+        if extent[dir] > 1
+            kern = _directional_kernel(dir, extent, method)
+
+            # compute gradient at specified points
+            A = zeros(kern)
+            shape = size(kern)
+            for (k, p) in enumerate(points)
+                icenter = CartesianIndex(ind2sub(extent, p))
+                i1 = CartesianIndex(tuple(ones(Int, ndirs)...))
+                for ii in CartesianRange(shape)
+                    A[ii] = imgpad[ii + icenter - i1]
+                end
+
+                G[k,dir] = sum(kern .* A)
+            end
+        end
+    end
+
+    G
+end
+
+function imgradients{T,N}(img::AbstractArray{T,N}; method::AbstractString="ando3", border::AbstractString="replicate")
+    extent = size(img)
+    ndirs = length(extent)
+
+    # pad input image only on appropriate directions
+    imgpad = _gradientpad(img, border)
+
+    # gradient tuple
+    Tret = typeof(zero(T)*zero(Float64))
+    G = Array(Array{Tret,N}, ndirs)
+
+    for dir in 1:ndirs
+        # kernel = centered difference + perpendicular smoothing
+        if extent[dir] > 1
+            kern = _directional_kernel(dir, extent, method)
+            G[dir] = imfilter(imgpad, kern, "inner")
+        end
+    end
+
+    result = (G...,)
+    if ndims(img) == 2 && spatialorder(img) == yx
+      result = (result[2], result[1])
+    end
+
+    result
+end
