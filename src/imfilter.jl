2c39b08d8d4fc3941e7d753332f1b8afc3e7d7ee
Jingpeng Wu <jingpeng.wu@gmail.com>
2018-01-17 11:35:21 -0500

    Update imfilter.jl

--- src/imfilter.jl
+++ src/imfilter.jl
@@ -98,7 +98,7 @@ You can also dispatch to different implementations by passing in a
 resource `r` as defined by the ComputationalResources package.  For
 example,
 
-    imfilter(ArrayFireLibs(), img, kernel)
+    imfilter(ArrayFireLibs(Algorithm.FIR()), img, kernel)
 
 would request that the computation be performed on the GPU using the
 ArrayFire libraries.
