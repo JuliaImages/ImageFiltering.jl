011d1cb107982dd81ddd9d7e4498ca08479934c0
JohnnyChen <johnnychen94@hotmail.com>
2018-10-13 02:50:18 +0800

    Correct indent style

--- src/fft.jl
+++ src/fft.jl
@@ -1,4 +1,3 @@
-
 """
     otf = psf2otf(psf, [outsize = size(psf)])
     otf = psf2otf(psf, dims...)
@@ -10,19 +9,19 @@
     See also: [`otf2psf`](@ref).
 """
 function psf2otf(psf::AbstractArray{T,N}, outsize::Tuple{Vararg{Integer}}=size(psf)) where {T,N}
-  psf = repeat(psf, ones(Int,length(outsize))...) # reshape the dimension
-  psfsize = size(psf)
+    psf = repeat(psf, ones(Int,length(outsize))...) # make dimension consistent
+    psfsize = size(psf)
 
-  if psfsize != outsize
-    pad = outsize .- psfsize
-    all(x -> x >= 0, pad) || throw(DimensionMismatch("psf $psfsize too large for output otf $outsize."))
-    lo_dim = tuple(zeros(Int,length(pad))...)
-    hi_dim = pad
-    psf = padarray(psf,Fill(zero(T),lo_dim,hi_dim))
-  end
+    if psfsize != outsize
+        pad = outsize .- psfsize
+        all(pad .>= 0) || throw(DimensionMismatch("psf $psfsize too large for output otf $outsize."))
+        lo_dim = tuple(zeros(Int,length(pad))...)
+        hi_dim = pad
+        psf = padarray(psf,Fill(zero(T),lo_dim,hi_dim))
+    end
 
-  shift = @. -floor(Int, psfsize/2)
-  fft(circshift(psf, shift))
+    shift = @. -floor(Int, psfsize/2)
+    fft(circshift(psf, shift))
 end
 psf2otf(psf::AbstractArray, outsize::Integer...) = psf2otf(psf, tuple(outsize...))
 
@@ -36,14 +35,14 @@ psf2otf(psf::AbstractArray, outsize::Integer...) = psf2otf(psf, tuple(outsize...
     See also: [`psf2otf`](@ref).
 """
 function otf2psf(otf::AbstractArray{T,N}, outsize::Tuple{Vararg{Integer}}=size(otf)) where {T,N}
-  otfsize = size(otf)
-  outsize = (outsize..., 
-        tuple(ones(Int, length(otfsize) - length(outsize))...)...)
-  all(otfsize >= outsize ) || throw(DimensionMismatch("output psf $outsize too large for otf $otfsize."))
+    otfsize = size(otf)
+    outsize = (outsize..., 
+        tuple(ones(Int, length(otfsize) - length(outsize))...)...) # make dimension consistent
+    all(otfsize >= outsize ) || throw(DimensionMismatch("output psf $outsize too large for otf $otfsize."))
 
-  psf = ifft(otf)
-  shift = @. floor(Int, outsize/2)
-  psf = circshift(psf,shift)
-  psf[map(x -> 1:x, outsize)...]
+    psf = ifft(otf)
+    shift = @. floor(Int, outsize/2)
+    psf = circshift(psf,shift)
+    psf[map(x -> 1:x, outsize)...]
 end
 otf2psf(otf::AbstractArray, outsize::Integer...) = otf2psf(otf, tuple(outsize...))
