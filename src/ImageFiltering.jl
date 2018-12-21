29a873a96fdbc5ff16a38dbfa69674b8e4a70c85
Tim Holy <tim.holy@gmail.com>
2018-12-21 09:08:14 -0600

    HACK: fix broadcasting on julia 1.0.3

--- src/ImageFiltering.jl
+++ src/ImageFiltering.jl
@@ -16,6 +16,13 @@ FixedColorant{T<:Normed} = Colorant{T}
 StaticOffsetArray{T,N,A<:StaticArray} = OffsetArray{T,N,A}
 OffsetVector{T} = OffsetArray{T,1}
 
+# Add a fix that should have been included in julia-1.0.3
+if isdefined(Broadcast, :_sametype) && !isdefined(Broadcast, :axistype)
+    axistype(a::T, b::T) where T = a
+    axistype(a, b) = UnitRange{Int}(a)
+    Broadcast._bcs1(a, b) = Broadcast._bcsm(b, a) ? axistype(b, a) : (Broadcast._bcsm(a, b) ? axistype(a, b) : throw(DimensionMismatch("arrays could not be broadcast to a common size")))
+end
+
 # Needed for type-stability
 function Base.transpose(A::StaticOffsetArray{T,2}) where T
     inds1, inds2 = axes(A)
