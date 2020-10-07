bd45241691ef908cc5761b97a0ca5b94e88b17d9
Deeptendu Santra <santradibbo@gmail.com>
2020-10-08 02:28:00 +0530

    Fixed typos,naming_convention, demo_cards

--- docs/demos/filters/min_max_filter.jl
+++ docs/demos/filters/min_max_filter.jl
@@ -1,56 +1,72 @@
-# Topic : Max Min Filters
-# reference paper : https://core.ac.uk/download/pdf/194053536.pdf
-
+# ---
+# title: Max min filters
+# cover: assets/max_min.gif
+# author: Deeptendu Santra
+# date: 2020-10-08
+# ---
 
 # In this tutorial we see how can we can effectively use max and min filter to distinguish 
-# between ramp("smooth") and ripple("texture") edges of B&W image.We will using the mapwindow function of
-# ImageFiltering.jl package.
+# between ramp/smooth and ripple/texture edges  in grayscale images.
 
-# Note : If you are using the terminal use imshow(<Image var. name>) to view the image. 
+# We will be using the [`mapwindow`](@ref) in `ImageFiltering.jl` which provides a genreal 
+# functionality to apply any function to the window around each pixel. 
 
-using Images, ImageFiltering
+using ImageCore,ImageShow, ImageFiltering
+using TestImages
 
-# We download an image from the internet and convert it into Grayscale image.
-# To work with your own image, use :
-# img_path = "<FULL PATH TO THE IMAGE>"
+img = Gray.(testimage("lighthouse"))      #Original Image       
 
-img_path = download("https://i.pinimg.com/originals/01/bc/8d/01bc8d82a3e2b4fa869f478479b97a3f.png")
-Img = Gray.(load(img_path));       
+# We can use a minimum function to check the minimum of GrayScale Values in the given matrix,or array
+# . For Example : 
+minimum([Gray(0.7),Gray(0.5),Gray(0.0)]) ##Should return Gray(0.0) i.e black.
+# 
+## Using the mapwindow, we create an image min.
+## The mapwindow maps the function over a moving window of given size
+img_min = mapwindow(minimum, img, (3, 3))
+## Similarly for maximum
+img_max = mapwindow(minimum, img, (3, 3))
+## The max(min) filter
+img_max_min = mapwindow(maximum, img_min, (3, 3))
+## The min(max) filter
+img_min_max = mapwindow(minimum, img_max, (3, 3))
+mosaicview(img_min, img_max, img_max_min, img_min_max; nrow = 1)
 
-# We will use a minimum function to check the minimum of GrayScale Values in the given matrix,or array
-# For Example
-minimum([Gray(0.7),Gray(0.5),Gray(0.0)]) #Should reutrn Gray(0.0) i.e black.
-
-# Using the mapwindow, create an image min.
-Min = mapwindow(minimum, Img, (5, 5))
-
-# Similarly for maximum
-Max = mapwindow(minimum, Img, (5, 5))
-
-# The max(min) filter
-Upp = mapwindow(maximum, Min, (9, 9))
-
-# The min(max) filter
-Low = mapwindow(minimum, Max, (9, 9))
-
-# No that we are done with the basic filtered images, we proceed to the next part
+# Now that we are done with the basic filtered images, we proceed to the next part
 # which is edge detection using these filters.
 
-# The trick is we define some thresholds.The thresholding can be dynamic for detecting
-# smooth parts or it can be texture based thresholding for detecting ripple edges.
+# For edge detection we need define thresholds for our image.Threshold is an important tool 
+# for binarizing a grayscale image.The threshold value for a given pixel practically
+# decides if the pixel visible or not in the output image.Appyling a global threshold might underconsider the 
+# variation of colors/brightness within the image.So we consider an adaptive type theresholding.
 
-Dyt = (Max + Min)./ 2   # Dynamic Threshold(DYT)
+# The max(min) and min(max) filter effectively follows the smooth edges of the image.So consequently,
+# their average also follows the smooth parts of the image.If we use this image as a threshold for the original
+# image, the smooth parts of the original image gets filtered out, leaving only the texture and/or noise behind.
+# So we can use  the average of `img_max_min` and `img_min_max` as the texture threshold.
 
-Tet = (Upp + Low)./ 2   # Texture Threshold(TET)
+# The average of min and max filters also gives us the smooth edges, but it also includes the noise in the image. 
+# So using average of `img_min` and `img_max` as a threshold  will yeild only texture.
 
-# The Dynamic Gist(more focus on the the smooth edges)
-Dyg = Img - Dyt
+img_text_noise_threshold = (img_max_min + img_max_min)./ 2
+img_text_threshold = (img_max + img_min)./2
+mosaicview(img_text_noise_threshold, img_text_threshold; nrow = 1)
+#
+## The Dynamic Gist is obtained by subtracting the img_text_threshold from the original image.
+## The filtered image gives us the texture of the image.
+img_dynamic_gist = img - img_text_threshold
+## The Texture Gist is obtained by subtracting img_text_noise_threshold from the original image.
+## The filtered image gives us the texture along with the noise of the image.
+img_texture_gist = img - img_text_noise_threshold
+mosaicview(img_dynamic_gist, img_texture_gist; nrow = 1)
+#
+ramp = img_dynamic_gist - img_texture_gist
+## Filtered Out edges
+edge = img - img_min_max
+## Smoothed our version of edge
+edge_smoothed = img_max_min - img_min_max
+mosaicview(img, ramp, edge, edge_smoothed; nrow = 2)
 
-# The Texture Gist( isolates out the textures and/or noise)
-Teg = Img - Tet
-
-# Filtered Out edges
-Edge = Img - Low
-
-# Smoothed our version of edge
-Edge_smoothed = Upp - Low
+## save covers #src
+using ImageMagick #src
+mkpath("assets")  #src
+ImageMagick.save("assets/max_min.gif",cat(img,edge,edge_smoothed,map(clamp01nan,noise);dims=3);fps =1) #src
