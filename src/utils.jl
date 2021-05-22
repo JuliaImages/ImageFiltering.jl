c4361a64622ae5382b0b5a7c9f4babd08da77294
Johnny Chen <johnnychen94@hotmail.com>
2021-05-23 05:15:27 +0800

    use OffsetArrays.centered

--- src/utils.jl
+++ src/utils.jl
@@ -1,22 +1,4 @@
 """
-    shiftedkernel = centered(kernel)
-
-Shift the origin-of-coordinates to the center of `kernel`.
-The center-element of `kernel` will be accessed by `shiftedkernel[0, 0, ...]`.
-
-This function makes it easy to supply kernels using regular Arrays,
-and provides compatibility with other languages that do not support
-arbitrary axes.
-
-See also: [`imfilter`](@ref).
-"""
-function centered(A::AbstractArray)
-    offsetted = first.(axes(A)) .- 1
-    total_offsets = .-((size(A) .+ 1 ) .÷ 2)
-    OffsetArray(A, total_offsets .- offsetted)
-end
-
-"""
     kernfft = freqkernel([T::Type], kern, sz=size(kern); rfft=false)
 
 Return a frequency-space representation of `kern`.
